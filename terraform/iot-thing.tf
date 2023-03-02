cc6a4a1e71a1bfdc971204c273340b9c322aa01b
Douglas Reith <douglas@reith.com.au>
2023-03-02 22:26:20 +1100

    TF providers have improved.

--- terraform/iot-thing.tf
+++ terraform/iot-thing.tf
@@ -39,3 +39,17 @@ resource "aws_iot_policy_attachment" "att" {
   policy = aws_iot_policy.growatt-to-iot.name
   target = var.aws_iot_certificate_arn
 }
+
+resource "aws_iot_thing_group" "growatt-to-iot-group" {
+  name = "growatt-to-iot-group"
+}
+
+resource "aws_iot_thing_group_membership" "growatt-to-iot-group-membership" {
+  thing_name       = aws_iot_thing.growatt-to-iot.name
+  thing_group_name = aws_iot_thing_group.growatt-to-iot-group.name
+}
+
+resource "aws_iot_logging_options" "growatt-to-iot-logging" {
+  default_log_level = "WARN"
+  role_arn          = aws_iam_role.growatt-to-iot-assume-role.arn
+}
